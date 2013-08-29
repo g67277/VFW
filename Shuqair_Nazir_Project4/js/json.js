2d9fd5be50b6aafe1b5ededf66a9a5d893b159e1
g67277 <nshuqair@fullsail.edu>
2013-08-28 20:07:22 -0400

    Added project 4
    JSON not communicating with main.js??

---
+++ Shuqair_Nazir_Project4/js/json.js
@@ -0,0 +1,30 @@
+// JavaScript Document
+
+var json = {
+	"conference1": {
+		"date": ["Date: ", "2013-08-21"], 		
+		"nameM": ["Name of the meeting: ", "COS Meeting"], 	
+		"poc": ["Contact Number: ", "702-221-2222"],		
+		"roomN": ["Room Number: ", "1NW307"], 		
+		"meetingT": ["Meeting Time: ", "1:30pm"], 	
+		"checkb": ["Capabilities Needed: ", "yes"], 	
+		"bridges": ["Bridge: ", "USAMTIC Audio"], 	
+		"code": ["Call code: ", "32435#"], 		
+		"length": ["Length of Meeting: ", "2hr"], 	
+		"userR": ["Did the user respond: ", "no"], 
+		"notes": ["Notes", "Nothing here"]		
+	},
+	"conference2": {
+		"date": ["Date: ", "2014-05-25"], 		
+		"nameM": ["Name of the meeting: ", "M6 Meeting"], 	
+		"poc": ["Contact Number: ", "702-221-3333"],		
+		"roomN": ["Room Number: ", "1NW439"], 		
+		"meetingT": ["Meeting Time: ", "3:30pm"], 	
+		"checkb": ["Capabilities Needed: ", "no"], 	
+		"bridges": ["Bridge: ", "TMS Video"], 	
+		"code": ["Call code: ", "55555#"], 		
+		"length": ["Length of Meeting: ", "1hr"], 	
+		"userR": ["Did the user respond: ", "yes"], 
+		"notes": ["Notes", "Something here"]		
+	}
+}
